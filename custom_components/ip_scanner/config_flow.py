03a45c715a1c11058d0412d6f3c6e5f9770a0a5d
SoFarSoGood86 <sofarsogood@live.fr>
2025-06-03 13:36:28 +0200

    Add files via upload

--- custom_components/ip_scanner/config_flow.py
+++ custom_components/ip_scanner/config_flow.py
@@ -1,20 +1,23 @@
-"""Config flow for IP Scanner integration."""
 from homeassistant import config_entries
-import voluptuous as vol
+from homeassistant.core import callback
+from .const import DOMAIN
 
-DOMAIN = "ip_scanner"
-
-@config_entries.HANDLERS.register(DOMAIN)
 class IPScannerConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    """Handle a config flow for IP Scanner."""
-
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
         if user_input is not None:
             return self.async_create_entry(title="IP Scanner", data={})
+        return self.async_show_form(step_id="user", data_schema=None)
 
-        return self.async_show_form(
-            step_id="user",
-            data_schema=vol.Schema({}),
-        )
+    @staticmethod
+    @callback
+    def async_get_options_flow(config_entry):
+        return IPScannerOptionsFlowHandler(config_entry)
+
+class IPScannerOptionsFlowHandler(config_entries.OptionsFlow):
+    def __init__(self, config_entry):
+        self.config_entry = config_entry
+
+    async def async_step_init(self, user_input=None):
+        return self.async_create_entry(title="", data={})
